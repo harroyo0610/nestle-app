972bb66fc6d5eaf8a768dcf9935dadc5ed1b1483
humberto <humberto-_-lzc@hotmail.com>
2020-08-18 22:07:59 -0500

    add credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
-    respond_to :json
 
   protected
 
