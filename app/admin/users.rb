cb731f0a09de861da8344db819a123312701e005
humberto <humberto-_-lzc@hotmail.com>
2020-08-22 21:02:37 -0500

    Add i18n spanish

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,6 @@ ActiveAdmin.register User do
     selectable_column
     id_column
     column :username
-    column :current_sign_in_at
     column :sign_in_count
     column :created_at
     column :role
