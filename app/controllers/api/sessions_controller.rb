283421ec261b1eba10308aaab70b82804960865b
humberto <humberto-_-lzc@hotmail.com>
2020-08-30 22:44:15 -0500

    add currently user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,27 +1,30 @@
 module Api
-    class SessionsController < Devise::SessionsController
+  class SessionsController < Devise::SessionsController
 
-        def create
-            self.resource = warden.authenticate!(auth_options)
-            set_flash_message!(:notice, :signed_in)
-            sign_in(resource_name, resource)
-            yield resource if block_given?
+      def create
+          self.resource = warden.authenticate!(auth_options)
+          set_flash_message!(:notice, :signed_in)
+          sign_in(resource_name, resource)
+          yield resource if block_given?
 
-            render json: { 'user': resource, 'sign_in_count': resource.sign_in_count }
-        end
+          render json: { 'user': resource, 'sign_in_count': resource.sign_in_count }
+      end
 
-        private
-    
-        def respond_with(resource, _opts = {})
-            if resource.id.nil? 
-                render json: { "error": "No hay usuarios con los datos que ingresó" }
-            else
-                render json: resource
-            end
-        end
-    
-        def respond_to_on_destroy
-            render json: { "response": true }
-        end
-    end
+      def signed_in
+        render json: { "response": user_signed_in? }
+      end
+      private
+  
+      def respond_with(resource, _opts = {})
+          if resource.id.nil? 
+              render json: { "error": "No hay usuarios con los datos que ingresó" }
+          else
+              render json: resource
+          end
+      end
+  
+      def respond_to_on_destroy
+          render json: { "response": true }
+      end
+  end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,33 +1,33 @@
 module Api
-    class UsersController < ApplicationController
-        def update
-          user = User.find(params[:user][:id])
-          if user.update(user_params)
-            render json: { 'response': "#{user.username} info successfully updated" }
-          else
-            render json: { 'response': "There was as error" }
-          end
+  class UsersController < ApplicationController
+      def update
+        user = User.find(params[:user][:id])
+        if user.update(user_params)
+          render json: { 'response': "#{user.username} info successfully updated" }
+        else
+          render json: { 'response': "There was as error" }
         end
+      end
+    
+      def get_user_type
+        user = User.find(params[:id])
+        render json: { 'role': user.role }
+      end
+
+      def get_user_by_username
+        user = User.find_by(username: params[:username])
+        render json: user
+      end
+
+      def get_user_by_id
+        user = User.find(params[:id])
+        render json: user
+      end
+
+      private
       
-        def get_user_type
-          user = User.find(params[:id])
-          render json: { 'role': user.role }
-        end
-
-        def get_user_by_username
-          user = User.find_by(username: params[:username])
-          render json: user
-        end
-
-        def get_user_by_id
-          user = User.find(params[:id])
-          render json: user
-        end
-
-        private
-        
-        def user_params
-            params.require(:user).permit(:id, :username, :gravatar_url, :password, :password_confirmation)
-        end
-    end
+      def user_params
+          params.require(:user).permit(:id, :username, :gravatar_url, :password, :password_confirmation)
+      end
+  end
 end
