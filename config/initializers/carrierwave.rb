f5d3b767a9344e75fe3a84e10aa82291c2ce7420
humberto <humberto-_-lzc@hotmail.com>
2020-09-01 22:56:27 -0500

    add current user

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: env['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: env['AWS_ACCESS_KEY_ID'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_ACCESS_KEY_ID'],
     region: 'us-east-2',
     path_style: true
   }
